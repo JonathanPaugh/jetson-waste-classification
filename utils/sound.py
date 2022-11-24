17df51d861787e816e45c86a87c2f0db514960dd
Jeff Phan <jeffphan99@gmail.com>
2022-11-24 13:55:40 -0800

    added absolute path variable to sound.py

--- utils/sound.py
+++ utils/sound.py
@@ -2,6 +2,8 @@ from playsound import playsound
 
 # mp3 files created from: https://ttsmp3.com/
 
+MP3_PATH = 'aboslute path to mp3 folder'
+
 sort_colour = {
     'glass': ['blue','bin'],
     'metal': ['blue','bin'],
@@ -13,9 +15,9 @@ sort_colour = {
 
 def play_sound(item_class):
 
-    playsound(f'utils/mp3/{sort_colour[item_class][0]}.mp3')
+    playsound(f'{MP3_PATH}{sort_colour[item_class][0]}.mp3')
 
-    playsound(f'utils/mp3/{sort_colour[item_class][1]}.mp3')
+    playsound(f'{MP3_PATH}{sort_colour[item_class][1]}.mp3')
 
 
 play_sound('trash')
