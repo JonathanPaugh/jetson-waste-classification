7b4529f19ed808e4c08668d2b96f0a20e6198501
Brandon Semilla <semibran@gmail.com>
2022-11-26 20:28:52 -0800

    first pass on fine tuning workflow

--- configs/model.py
+++ configs/model.py
@@ -14,7 +14,8 @@ VALIDATION_SPLIT = 0.2
 IMAGE_SIZE = (224, 224)  # NOTE: should be compatible with expected feature extractor inputs
 
 MODEL_FEATURE_EXTRACTOR = 'https://tfhub.dev/google/tf2-preview/mobilenet_v2/feature_vector/4'
-MODEL_NUM_EPOCHS = 10
+MODEL_NUM_EPOCHS = 20
+MODEL_NUM_FINE_TUNE_EPOCHS = 10
 MODEL_EARLY_STOPPING_PATIENCE = 3  # stop after x consecutive epochs with no improvement
 MODEL_WORKERS = os.cpu_count() | 1
 MODEL_BATCH_SIZE = int(math.pow(2, 3)) if IS_JETSON else int(math.pow(2, 6))
--- core/model.py
+++ core/model.py
@@ -1,6 +1,7 @@
 from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, \
     InputLayer, Rescaling, RandomFlip, RandomRotation, RandomZoom
 from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras.callbacks import EarlyStopping
 from tensorflow_hub import KerasLayer
 from utils.pickle import has_trained_model, import_trained_model, export_trained_model
@@ -39,22 +40,54 @@ def compile_model(num_classes):
     return model
 
 
+def recompile_model_for_fine_tuning(model):
+    feature_extractor = next((l for l in model.layers
+        if isinstance(l, KerasLayer)), None)
+    if feature_extractor is None:
+        return False  # fail silently if no feature extractor found
+
+    feature_extractor.trainable = True
+    model.summary()
+    model.compile(
+        optimizer=adam_v2.Adam(learning_rate=1e-5),
+        loss='sparse_categorical_crossentropy',
+        metrics=['accuracy'],
+    )
+    return True
+
+
+def _build_fit_params(**kwargs):
+    return {
+        'batch_size': config.MODEL_BATCH_SIZE,
+        'workers': config.MODEL_WORKERS,
+        'use_multiprocessing': True,
+        **kwargs,
+    }
+
+
 def train_model(model, train_data, test_data, use_import=True, use_export=True):
     if use_import and has_trained_model():
         return import_trained_model(model)
 
+    fit_params = _build_fit_params(validation_data=test_data)
+
     _history = model.fit(
         train_data,
-        validation_data=test_data,
         epochs=config.MODEL_NUM_EPOCHS,
-        batch_size=config.MODEL_BATCH_SIZE,
-        workers=config.MODEL_WORKERS,
-        use_multiprocessing=True,
-        callbacks=[EarlyStopping(
-            monitor='loss',
-            patience=config.MODEL_EARLY_STOPPING_PATIENCE,
-        )]
+        **fit_params
     )
+
+    if recompile_model_for_fine_tuning(model):
+        _history = model.fit(
+            train_data,
+            epochs=config.MODEL_NUM_EPOCHS + config.MODEL_NUM_FINE_TUNE_EPOCHS,
+            initial_epoch=config.MODEL_NUM_EPOCHS,
+            callbacks=[EarlyStopping(
+                monitor='loss',
+                patience=config.MODEL_EARLY_STOPPING_PATIENCE,
+            )],
+            **fit_params,
+        )
 
     history = _history.history
     if use_export:
