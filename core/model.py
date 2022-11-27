a8b83bb5eefd433d305891f39cce336871706652
Brandon Semilla <semibran@gmail.com>
2022-11-26 17:37:12 -0800

    add data augmentation

--- core/model.py
+++ core/model.py
@@ -1,20 +1,28 @@
-from tensorflow.python.keras.layers import Rescaling, MaxPooling2D, Flatten, Dense, Conv2D
-from tensorflow.python.keras.losses import SparseCategoricalCrossentropy
+from tensorflow.python.keras.layers import Conv2D, MaxPooling2D, Dropout, Flatten, Dense, \
+    Rescaling, RandomFlip, RandomRotation, RandomZoom
 from tensorflow.python.keras.models import Sequential
-
 from utils.pickle import has_trained_model, import_trained_model, export_trained_model
 import configs.model as config
+
+
+data_augmentation = Sequential([
+  RandomFlip("horizontal_and_vertical"),
+  RandomRotation(0.2),
+  RandomZoom(0.1),
+])
 
 
 def compile_model(num_classes):
     model = Sequential([
         Rescaling(1./255),
+        data_augmentation,
         Conv2D(32, 3, activation='relu'),
         MaxPooling2D(),
         Conv2D(32, 3, activation='relu'),
         MaxPooling2D(),
         Conv2D(32, 3, activation='relu'),
         MaxPooling2D(),
+        Dropout(0.2),
         Flatten(),
         Dense(128, activation='relu'),
         Dense(num_classes, activation='softmax'),
@@ -45,4 +53,3 @@ def train_model(model, train_data, test_data, use_import=True, use_export=True):
             export_trained_model(model, history)
 
     return history
-
