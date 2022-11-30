4f1af4a21058262aca7574a7035410acf3db63a7
Brandon Semilla <semibran@gmail.com>
2022-11-30 02:35:02 -0800

    setup transfer learning wrapper

--- configs/model.py
+++ configs/model.py
@@ -1,6 +1,5 @@
 import os
 from os.path import exists, isfile
-from configs.transfer import config as config_transfer
 
 
 JETSON_FILE = '.jetson'
@@ -12,16 +11,12 @@ OUTPUT_PATH = 'dist'
 
 SEED = 0
 VALIDATION_SPLIT = 0.2
-IMAGE_SIZE = (384, 384)
+IMAGE_SIZE = (96, 96)
 
-MODEL_FEATURE_EXTRACTOR = None  # config_transfer(size=384)
+MODEL_EARLY_STOPPING_MONITOR = 'val_accuracy'
 MODEL_EARLY_STOPPING_PATIENCE = 7  # stop after x consecutive epochs with no improvement
-MODEL_NUM_EPOCHS = 50
-MODEL_FINE_TUNING = True
-MODEL_FINE_TUNING_NUM_EPOCHS = 50
-MODEL_FINE_TUNING_LEARNING_RATE = 1e-5         # defaults to 1e-4
-MODEL_FINE_TUNING_BATCH_NORM_MOMENTUM = 0.997  # defaults to 0.99
-MODEL_DROPOUT_RATE = 0.3
+MODEL_NUM_EPOCHS = 1
+MODEL_DROPOUT_RATE = 0.5
 MODEL_WORKERS = os.cpu_count() | 1
 MODEL_BATCH_SIZE = 2 ** (3 if IS_JETSON else 5)
 LOADER_BATCH_SIZE = 2 ** (3 if IS_JETSON else 5)
---
+++ models/tensorflow_hub.py
@@ -0,0 +1,26 @@
+"""
+Details layer-based transfer learning models from TensorFlow Hub.
+Note that using these models may require configuring specific image sizes in `configs/model.py`.
+"""
+
+from core.transfer_learning import LayerBasedTransferLearningModel
+
+
+class InceptionV3(LayerBasedTransferLearningModel):  # uses 299x299 by default (limited?)
+    handle = 'https://tfhub.dev/google/imagenet/inception_v3/feature_vector/5'
+
+class MobileNetV2x224(LayerBasedTransferLearningModel):
+    IMAGE_SIZE = (224, 224)
+    handle = 'https://tfhub.dev/google/imagenet/mobilenet_v2_100_224/feature_vector/5'
+
+class MobileNetV2x192(LayerBasedTransferLearningModel):
+    IMAGE_SIZE = (192, 192)
+    handle = 'https://tfhub.dev/google/imagenet/mobilenet_v2_100_192/feature_vector/5'
+
+class MobileNetV2x128(LayerBasedTransferLearningModel):
+    IMAGE_SIZE = (128, 128)
+    handle = 'https://tfhub.dev/google/imagenet/mobilenet_v2_100_128/feature_vector/5'
+
+class MobileNetV2x96(LayerBasedTransferLearningModel):
+    IMAGE_SIZE = (96, 96)
+    handle = 'https://tfhub.dev/google/imagenet/mobilenet_v2_100_96/feature_vector/5'
