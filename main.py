64bdafe613c146088521d70a6f04a523745c88bb
sniff <jpaughjape@gmail.com>
2022-11-29 20:22:40 -0800

    add jetson conditionals

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from core.loader import load_train_dataset
 from core.model import compile_model, train_model
 from utils_jetson.hardware import tweak_hardware_settings
-from utils.evaluation import get_evaluation_metrics
-
+from utils.evaluation import output_training_results
 
 def main():
     tweak_hardware_settings()
@@ -10,13 +9,7 @@ def main():
     train_data, test_data = load_train_dataset()
     model = compile_model(num_classes=len(train_data.class_names))
     history = train_model(model, train_data, test_data)
-
-    if not config.IS_JETSON:
-        plot_history(history)
-
-    evaluate_model(model, test_data)
-
-    get_evaluation_metrics(history, model, test_data)
+    output_training_results(history, model, test_data)
 
 
 if __name__ == '__main__':
